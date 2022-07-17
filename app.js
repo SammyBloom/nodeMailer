d59ca806ccc9956879fba3c7b2616cc067d81126
SammyBloom <nwachiemi@gmail.com>
2022-07-17 02:45:40 +0100

    Added structure to my folders. implemented the render method to ensure the required handlebar page is rendered. Added css and html to complete the visual experience of the form

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { ExpressHandlebars } = require('express-handlebars');
-const exphbs = require(express-ExpressHandlebars);
+const { engine } = require('express-handlebars');
+const path = require('path');
 const nodemailer = require('nodemailer');
 
 const app = express();
 
-app.get('/', (request, response) => {
-   response.send('Hello'); 
+// View Engine Setup
+app.engine('handlebars', engine({
+    layoutsDir: __dirname + "/views/layouts", extname: 'handlebars',
+}));
+app.set('view engine', 'handlebars');
+// app.set('views', './views');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+   res.render('main', {
+    layout: 'contact',
+   }); 
 });
 
 
